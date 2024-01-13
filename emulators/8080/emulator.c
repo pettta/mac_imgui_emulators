1073162f1d0b169cdad57b11f91c38a81dad6289
pettta <tp663@drexel.edu>
2024-01-13 15:11:47 -0500

    update structure to wrap disassembler in emulator

--- emulators/8080/emulator.c
+++ emulators/8080/emulator.c
@@ -1,6 +1,19 @@
 #include <stdio.h>
 #include <stdlib.h>
+#include <string.h>
 #include "disassembler.h"
+
+void UnimplementedInstruction(State8080* state) {
+    // pc will have advanced one, so undo that
+    printf ("Error: Unimplemented instruction\n");
+    exit(1);
+}
+
+void Emulate8080Op(State8080* state) {
+    unsigned char *opcode = &state->memory[state->pc];
+    state->pc += Disassemble8080Op(state->memory, state->pc);
+}
+
 
 int main (int argc, char** argv) {
     FILE *f = fopen(argv[1], "rb");
@@ -17,10 +30,14 @@ int main (int argc, char** argv) {
     fread(buffer, fsize, 1, f);
     fclose(f);
 
-    int pc = 0; // init prrogram counter
+    State8080* newState = (State8080*)malloc(sizeof(State8080));
+    newState->memory = malloc(0x10000);  //16K
+    memcpy(newState->memory, buffer, fsize);
+    newState->pc = 0;
+    
 
-    while (pc < fsize) {
-        pc += Disassemble8080Op(buffer, pc);
+    while (newState->pc < fsize) {
+        Emulate8080Op(newState);
     }
 
     return 0;
